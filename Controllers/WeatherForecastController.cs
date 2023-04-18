7a175d742988fa93bbe8acad59061d1da343aa3f
778116@cognizant.com <ammar.gangardiwala@cognizant.com>
2023-04-18 01:29:33 -0500

    all caps

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "FREEZING", "BRACING", "CHILLY", "COOL", "MILD", "WARM", "BALMY", "HOT", "SWELTERING", "SCORCHING"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
